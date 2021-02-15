66bdbed92545d376c792af8fa21aef54de162084
XeniaAllayarova <yoshasoul@gmail.com>
2021-02-15 10:06:34 +0500

    Update Поиск шаблонов-1.py

--- Поиск шаблонов-1.py
+++ Поиск шаблонов-1.py
@@ -2,14 +2,11 @@ from apyori import apriori
 
 filename = "retail.dat"
 Transactions = [i.strip().split() for i in open(filename).readlines()]
-apriori_results = list(apriori(Transactions,min_support=0.01,max_length=2))
+apriori_results = list(apriori(Transactions,min_support=0.1))
 
-number=1
 for item in range(0, len(apriori_results)):
     current=str(apriori_results[item]).split('=')
-    print(str(number)+") Набор:"+(current[1])[11:-11])
-    print("Поддержка:"+str(round(float((current[2])[:-20])*100,2))+"%")
+    print("Набор:"+(current[1])[11:-11],"Поддержка:"+str(round(float((current[2])[:-20])*100,2))+"%")
     if (str(round(float((current[2])[:-20])*100,2))!=str(round(float((current[6])[:-6]) * 100, 2))):
         print("Достоверность:" + str(round(float((current[6])[:-6]) * 100, 2)) + "%")
-    print("\n")
-    number=number+1
+    print()
